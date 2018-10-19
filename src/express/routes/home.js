78ba622a8712b9e77d729599fc4e8bae307d4b6f
András Bácsai <andrasbacsai@users.noreply.github.com>
2018-10-19 23:40:30 +0200

    Update home.js

--- src/express/routes/home.js
+++ src/express/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send('return')
+    res.status(200).send('ready')
 });
 
 module.exports = router;
