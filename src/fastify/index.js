6c28014ac4e356399079e21bd944f1fb22b073ff
alicankustemur <alican.kustemur@gmail.com>
2018-10-20 14:19:51 +0300

    Revert port as 8080

--- src/fastify/index.js
+++ src/fastify/index.js
@@ -9,7 +9,7 @@ fastify.register(home)
 
 const start = async () => {
   try {
-    await fastify.listen(8181,'0.0.0.0')
+    await fastify.listen(8080,'0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
