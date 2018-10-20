686e3311104e3e224f12b3f83efe2b714aa5e512
András Bácsai <andrasbacsai@users.noreply.github.com>
2018-10-20 11:31:35 +0200

    Update index.js

--- src/express/index.js
+++ src/express/index.js
@@ -10,4 +10,4 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', home);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
