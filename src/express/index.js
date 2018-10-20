5bb4f60d1e96eb626bebcb3169d6fa69288a26cb
András Bácsai <andrasbacsai@users.noreply.github.com>
2018-10-20 11:30:01 +0200

    Update index.js

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const home = require('./routes/home');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));   
 
 app.use('/', home);
 
